72177873199d47351f606b9df33dbedae1cb78c9
= <dineshcrj1301@gmail.com>
2026-01-01 03:12:43 +0530

    update5:01.01.26

--- src/main/java/com/telusko/RestDemo/controller/StudentController.java
+++ src/main/java/com/telusko/RestDemo/controller/StudentController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import com.telusko.RestDemo.model.Student;
 import com.telusko.RestDemo.service.StudentService;
 
-import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping("/students")
@@ -25,15 +24,15 @@ public class StudentController {
         return service.getAllStudents();
     }
 
-    // @DeleteMapping("/{id}")
-    // public List<Student> deleteStudents(@PathVariable int id) {
-    //     service.deleteStudentById(id);
-    //     System.out.println(id);
-    //     List<Student> list = service.getAllStudents();
-    //     Student st1 = new Student();
-    //     st1.setName("updated");
-    //     st1.setAge(2026);
-    //     list.add(st1);
-    //     return list;
-    // }
+    @DeleteMapping("/{id}")
+    public List<Student> deleteStudents(@PathVariable int id) {
+        service.deleteStudentById(id);
+        System.out.println(id);
+        List<Student> list = service.getAllStudents();
+        Student st1 = new Student();
+        st1.setName("updated");
+        st1.setAge(2026);
+        list.add(st1);
+        return list;
+    }
 }
